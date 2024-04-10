88f7b4db2955707c851a82ad289cf9497d6e7194
Reapser <114721203+Reapser@users.noreply.github.com>
2024-04-10 10:34:28 +0300

    Add files via upload

---
+++ PR2/Prac 2.1/Prac 2.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Pr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Введiть 3 цiлих числа:");
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int No = 8;
+
+            Console.WriteLine("Числа якi належать iнтервалу [1,10+No]:");
+            if (num1 >= 1 && num1 <= (10 + No))
+            {
+                Console.WriteLine(num1);
+            }
+            if (num2 >= 1 && num2 <= (10 + No))
+            {
+                Console.WriteLine(num2);
+            }
+            if (num3 >= 1 && num3 <= (10 + No))
+            {
+                Console.WriteLine(num3);
+            }
+        }
+    }
+
+}
---
+++ PR2/prac2.3/prac2.3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        int[] X = new int[10 + 8];
+
+
+        Random random = new Random();
+        for (int i = 0; i < X.Length; i++)
+        {
+            X[i] = random.Next(100);
+        }
+
+
+        Console.WriteLine("Масив X:");
+        foreach (int num in X)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+
+
+        int min = X[0];
+        int max = X[0];
+        for (int i = 1; i < X.Length; i++)
+        {
+            if (X[i] < min)
+            {
+                min = X[i];
+            }
+            if (X[i] > max)
+            {
+                max = X[i];
+            }
+        }
+
+
+        Console.WriteLine($"Мінімальне значення: {min}");
+        Console.WriteLine($"Максимальне значення: {max}");
+    }
+}
