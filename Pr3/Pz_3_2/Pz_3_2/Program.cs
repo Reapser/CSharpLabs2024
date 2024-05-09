c4294c943fed2e067b4ba94e596f483e13f11eb6
Reapser <114721203+Reapser@users.noreply.github.com>
2024-05-09 08:48:45 +0300

    Add files via upload

---
+++ Pr3/Pz_3_2/Pz_3_2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+public class Point
+{
+    private int x;
+    private int y;
+    private string name;
+
+    public int X => x;
+    
+
+    public int Y
+    {
+        get { return y; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+    }
+
+    public Point(int x, int y, string name)
+    {
+        this.x = x;
+        this.y = y;
+        this.name = name;
+    }
+}
+
+public class Figure
+{
+    private Point[] points;
+
+    public Figure(params Point[] points)
+    {
+        if (points.Length < 3 || points.Length > 5)
+            throw new ArgumentException("Фігура повинна мати від 3 до 5 points.");
+
+        this.points = points;
+    }
+
+
+
+    private double LengthSide(Point A, Point B)
+    {
+        return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+    }
+
+   
+
+    public void PerimeterCalculator()
+    {
+        double perimeter = 0;
+        for (int i = 0; i < points.Length; i++)
+        {
+            int nextIndex = (i + 1) % points.Length;
+            perimeter += LengthSide(points[i], points[nextIndex]);
+        }
+        Console.WriteLine($"Периметр фігури: {perimeter}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Point A = new Point(0, 0, "A");
+        Point B = new Point(3, 0, "B");
+        Point C = new Point(3, 4, "C");
+        Point D = new Point(0, 4, "D");
+
+        
+        Figure rectangle = new Figure(A, B, C, D);
+
+        
+        rectangle.PerimeterCalculator();
+        
+    }
+}
---
+++ Pr3/pz3/pz3/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+
+
+class Title
+{
+    private string title;
+
+    public Title(string title)
+    {
+        this.title = title;
+    }
+
+    public void Show()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Назва книги: " + title);
+        Console.ResetColor();
+    }
+}
+
+class Author
+{
+    private string authorName;
+
+    public string AuthorName
+    {
+        get { return authorName; }
+        set { authorName = value; }
+    }
+
+    public void Show()
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Автор: " + authorName);
+        Console.ResetColor();
+    }
+}
+
+
+class Content
+{
+    private string content;
+
+    public string ContentText
+    {
+        get { return content; }
+        set { content = value; }
+    }
+
+    public void Show()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Зміст: " + content);
+        Console.ResetColor();
+    }
+}
+
+
+class Book
+{
+    private Title title;
+    private Author author;
+    private Content content;
+
+    public Book(string bookTitle, string authorName, string contentText)
+    {
+        title = new Title(bookTitle);
+        author = new Author();
+        author.AuthorName = authorName;
+        content = new Content();
+        content.ContentText = contentText;
+    }
+
+    public void Show()
+    {
+        title.Show();
+        author.Show();
+        content.Show();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Book book = new Book("Тополі", "Баскервілі Ілі", "Фентезійний  пригодницький роман з елементами хоррору, комедії, ");
+        book.Show();
+        
+    }
+}
